dc7499c333ce1a67f5862fd5c4bd36de0ac5ab12
Mark Baum <markmbaum@protonmail.com>
2022-01-11 21:03:23 -0500

    unsure why test failed for J1.7

--- test/test_vandermonde.jl
+++ test/test_vandermonde.jl
@@ -1,7 +1,7 @@
 function vandermonde_error(n)
     rng = MersenneTwister(n)
     a = rand(rng, n)
-    x = rand(rng, n)
+    x = LinRange(-1, 1, n)
     y = zeros(n)
     for i = 1:n
         y[i] = sum(ntuple(j->a[j]*x[i]^(j-1), n))
